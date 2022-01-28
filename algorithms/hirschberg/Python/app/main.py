e438348b34e510d82cc87c1e0ab9b1733db38281
Leonardo Rebouças de Carvalho <leonardo_r_carvalho@yahoo.com.br>
2022-01-28 18:17:51 -0300

    typos

--- algorithms/hirschberg/Python/app/main.py
+++ algorithms/hirschberg/Python/app/main.py
@@ -39,7 +39,7 @@ def align( event ):
     s2 = list(_s2)
     started_at = datetime.datetime.now()
     bucket = event["bucket"]
-    if !s3.Bucket(bucket).creation_date:
+    if not s3_client.Bucket(bucket).creation_date:
         s3_client.create_bucket(Bucket=bucket)
     
     result = {
